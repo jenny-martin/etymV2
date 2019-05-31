1dce7c8d6e0f24b7984ecd365b70848485a05932
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-05-30 22:03:23 -0500

    first commit v2

---
+++ server.js
@@ -0,0 +1,66 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
+
+//load env 
+require('dotenv').config();
+
+var app = express();
+
+// connect to the MongoDB with mongoose
+require('./config/database');
+
+//configure passport 
+require('./config/passport');
+
+//require routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'Wordswordswords!',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+
+//mount the routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/words', wordsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
